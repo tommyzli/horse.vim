e1cd0f513a536496831c64594cdd84199e41e84e
Tommy Li <tommyzli@gmail.com>
2017-05-16 19:35:46 -0400

    Clone over ssh, use new Vundle url

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-git clone https://github.com/tommyzli/horse.vim.git
+git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
+git clone git@github.com:tommyzli/horse.vim.git
 ln -sf ~/horse.vim/.vimrc ~/.vimrc
 ln -sf ~/horse.vim/.vimrc.bundles ~/.vimrc.bundles
 vim +PluginInstall +qall
