345a4967411f8d317704c02367215cf131768e08
Tommy Li <tommyzli@gmail.com>
2024-12-28 10:48:30 -0500

    pull colorscheme

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 brew install fzf bat ripgrep the_silver_searcher perl universal-ctags
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/colors/monokai.vim --create-dirs https://raw.githubusercontent.com/ku1ik/vim-monokai/refs/heads/master/colors/monokai.vim
 ln -sf ~/horse.vim/.vimrc ~/.vimrc
 vim +PlugInstall +qall
